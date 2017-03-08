fa97491ede26b295830abd04467cf49431364915
aalshawa97 <abdullahalshawa@gmail.com>
2017-03-08 12:38:24 -0800

    RSA Encryption
    Working on RSA Encryption

---
+++ app/src/main/java/com/example/abdul/encryptedmessenger/RSAencryption.java
@@ -0,0 +1,26 @@
+package com.example.abdul.encryptedmessenger;
+
+import android.app.Activity;
+import android.util.Log;
+
+/**
+ * Created by abdul on 3/8/2017.
+ */
+
+public class RSAencryption extends Activity
+{
+    long Encrypt(long plainText,long n,  long e)
+    {
+        long encryptedPlainText = 1;
+
+        for (long i = 0; i < e; i ++)
+        {
+            encryptedPlainText = ((encryptedPlainText * plainText) % n);
+        }
+
+
+        return encryptedPlainText;
+
+
+    }
+}
