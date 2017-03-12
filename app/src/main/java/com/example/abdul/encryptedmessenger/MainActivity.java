475c1f44a8aa50cea2a16ec21a1f2c8f4fd67365
aalshawa97 <abdullahalshawa@gmail.com>
2017-03-12 11:18:43 -0700

    Editing Values in the Firebase
    This is the first step for decryption. This function edits a value in
    the Firebase when the decryption button is clicked: public void
    displayDecryptedChatMessage( String valueKey).

--- app/src/main/java/com/example/abdul/encryptedmessenger/MainActivity.java
+++ app/src/main/java/com/example/abdul/encryptedmessenger/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.abdul.encryptedmessenger;
 import android.content.Context;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.annotation.StringDef;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -226,6 +227,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void displayDecryptedChatMessage( String valueKey)
     {
+        mDatabase.child("-Kf2j50AiX16sHpMFEV2").child("messageText").setValue(String.valueOf(0));
+        mDatabase.child("-Kf2j50AiX16sHpMFEV2").child("messageUser").setValue("Example of me editing this value");
+
         //Trying new method for decryption
 
         /*
